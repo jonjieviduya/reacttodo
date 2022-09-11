d8bf8e83b8f8b20163b239b9ac0c78f33dc3fc81
Alvin <alvinreggaelaroya@gmail.com>
2022-09-11 20:11:56 +0800

    chore: render component in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
+
+//components
+
+import Header from "./components/Header";
+import Form from "./components/Form";
+import Tabs from "./components/Tabs";
+import Todos from "./components/Todos";
 
 const App = () => {
   return (
     <section className="flex justify-center h-screen">
       <div className="border border-gray-400 rounded p-10 w-[942px] h-[300px] mt-10">
-
+        <Header />
+        <Form />
+        <Tabs />
+        <Todos />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default App
+export default App;
