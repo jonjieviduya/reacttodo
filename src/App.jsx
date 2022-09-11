b231e55a388ae559e922fcf374e859c9e29973df
Jonjie Viduya <jonjieviduya@gmail.com>
2022-09-11 19:51:21 +0800

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <section className="flex justify-center h-screen">
+      <div className="border border-gray-400 rounded p-10 w-[942px] h-[300px] mt-10">
+
+      </div>
+    </section>
+  )
+}
+
+export default App
