13e6fa5054275bfd9a35dad01b3a6bd7d716bdc5
Jonjie Viduya <jonjieviduya@gmail.com>
2022-09-11 20:19:57 +0800

    Added header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
-};
+const Header = () => (
+	<section className="mb-5">
+		<h2 className="text-2xl">To-Do List</h2>
+	</section>
+)
 
 export default Header;
