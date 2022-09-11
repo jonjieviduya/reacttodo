8954927105d897865181467c254a37796554c55b
Jonjie Viduya <jonjieviduya@gmail.com>
2022-09-12 00:50:32 +0800

    Updated the size of the container.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Todos from "./components/Todos";
 const App = () => {
   return (
     <section className="flex justify-center h-screen">
-      <div className="border border-gray-400 rounded p-10 w-[942px] mt-10">
+      <div className="border border-gray-400 rounded p-10 w-[700px] mt-10 mb-10">
         <Header />
         <Form />
         <Tabs />
