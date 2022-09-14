4be5c09892f0134455825f37423164a088027f90
Jonjie Viduya <jonjieviduya@gmail.com>
2022-09-14 20:16:55 +0800

    Created tab component.

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Tabs = () => {
-  return <div>Tabs</div>;
-};
+const Tabs = () => (
+  <section className="mb-5">
+    <div className="flex gap-2">
+      <div className="border-b-2 border-blue-600 pb-2 cursor-pointer">
+        All Tasks <small className="text-gray-600">(3)</small>
+      </div>
+      <div className="pb-2 cursor-pointer">
+        Completed <small className="text-gray-600">(0)</small>
+      </div>
+    </div>
+  </section>
+);
 
 export default Tabs;
